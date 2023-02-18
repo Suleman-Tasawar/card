e80d6b9a9564fd4d15e9e0302f01c9eb7647bc67
Suleman-Tasawar <sulemantasawar9@gmail.com>
2023-02-17 19:33:09 -0800

    Boo Yeah

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './style.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Card(){
+    return(
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        <div className='summary-card'>
+            <div className='card-upper-bg'></div>
+            <div className='card-content'>
+                <h1>Order Summary</h1>
+                <p>You can now listen to millions of songs,books
+                    ,audiobook and podcasts on any device anywhere you like
+                </p>
+            </div>
+            <div className='plan-info'>                
+                <div className='plan-details'>
+                    <div className='audio-icon'></div>
+                    <div>
+                        <div>
+                            <h1>Annual Plan</h1>
+                        </div>
+                        <div>
+                            59.99$/year
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <a className='plan-change-link' href='#'>Change</a>
+                </div>
+            </div>
+
+            <div>
+                <button>Proceed to Payement</button>
+            </div>
+
+            <div>
+                <button>Cancel Order</button>
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<Card />,document.getElementById("root"))
